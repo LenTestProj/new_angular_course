f57b92c3d9041bd6e1dbd65625010f95d85eb74b
LenTestProj <lennfernz2@gmail.com>
2024-07-22 05:26:51 +0530

    started with Section 2

---
+++ Section2_angular_essentials_components_templates_services/starting-project/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from './header.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone:true,
+  imports:[HeaderComponent]
+})
+export class AppComponent {
+
+
+  
+}
